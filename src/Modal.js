e596733fe1f8a45efb91978d5f2528d866f80845
Thomas Chamberlin <tchamber@nrao.edu>
2019-04-17 15:31:13 -0400

    codemod for React v16

--- src/Modal.js
+++ src/Modal.js
@@ -1,6 +1,8 @@
 'use strict';
 
-import React, {Component, PropTypes} from 'react';
+import PropTypes from 'prop-types';
+
+import React, { Component } from 'react';
 import classnames from 'classnames';
 
 class Modal extends Component {
